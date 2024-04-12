b5062cce64f37c67c913030d044839461f51f8e5
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-04-12 23:31:20 +0900

    Title: 행복한 수열의 개수, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240412/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240412/행복한 수열의 개수/number-of-happy-sequence.java
@@ -1,18 +1,20 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N, M;
     static StringTokenizer st;
     static int[][] map;
     static boolean[] v;
+    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         map = new int[N][N];
-        v = new boolean[N];
+        v = new boolean[100];
+        int answer = 0;
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<N;j++) {
@@ -20,24 +22,51 @@ public class Main {
             }
         }
 
-        for(int i=0;i<N;i++) { // 3
-            for(int j=0;j<N-M;j++) { // 1번
-                HashMap<Integer, Integer> hashMap = new HashMap<>();
-                for(int k=j;k<j+M+1;j++) { 
-                    hashMap.put(map[i][j], map.getOrDefault(map[i][j], 0) + 1);
+        // 행복한 행 구하기
+        for(int i = 0 ; i < N ; i++) {
+            int tmp = 1;    // 연속한 수 갯수
+            int beforeVal = map[i][0];    // 이전 값
+
+            for(int j = 1 ; j < N ; j++) {
+                if(map[i][j] == beforeVal) {
+                    tmp++;
                 }
-                Set<Integer> keySet = hashMap.keySet();
-                for(Integer key : keySet) {
-                    if(hashMap.get(key) >= M) 
-                       v[key] = true; 
+                else {
+                    beforeVal = map[i][j];
+                    tmp = 1;
+                }
+
+                // 연속한 수 갯수가 M개 이상이라면, 정답 +1하고 탈출
+                if(tmp >= M) {
+                    cnt++;
+                    break;
                 }
             }
         }
-        int answer = 0;
-        for(int i=0;i<v.length;i++) {
-            if(v[i]) 
-                answer++;
+
+
+        // 행복한 열 구하기
+        for(int i = 0 ; i < N ; i++) {
+            int tmp = 1;    // 연속한 수 갯수
+            int beforeVal = map[0][i];    // 이전 값
+
+            for(int j = 1 ; j < N ; j++) {
+                if(map[j][i] == beforeVal) {
+                    tmp++;
+                }
+                else {
+                    beforeVal = map[j][i];
+                    tmp = 1;
+                }
+
+                // 연속한 수 갯수가 M개 이상이라면, 정답 +1하고 탈출
+                if(tmp >= M) {
+                    cnt++;
+                    break;
+                }
+            }
         }
-        System.out.println(answer);
+
+        System.out.println(cnt);
     }
 }
