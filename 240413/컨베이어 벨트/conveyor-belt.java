0b19aa884fa91e18c69f23c828790a44fc4d4995
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-04-13 18:55:25 +0900

    Title: 컨베이어 벨트, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 240413/컨베이어 벨트/conveyor-belt.java
+++ 240413/컨베이어 벨트/conveyor-belt.java
@@ -2,70 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, T; // N : 개수, T : 시간 초
-    static int[][] map;
+    static int N, T;
+    static int[] u;
+    static int[] d;
     static StringTokenizer st;
-    static int time;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
-        time = 0;
-        map = new int[2][N];
-        for(int i=0;i<2;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+        u = new int[N];
+        d = new int[N];
+        int time = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            u[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            d[i] = Integer.parseInt(st.nextToken());
         }
 
         while(time != T) {
-            int temp = 0;
-            for(int i=0;i<2;i++) {
-                if(i == 0) {
-                    for(int j=0;j<N;j++) { // ex) 2면 0, 1만 진행
-                        // 초기 값일때, temp를 설정하고 시작해야함
-                        if(j == 0) {
-                            temp = map[i][j+1];
-                            map[i][j+1] = map[i][j];
-                        }
-                        // 마지막 열일 경우, 아래로 밀고 시간 초 증가
-                        else if(j == N-1) {
-                            int tmp = map[i+1][j-N+1];
-                            map[i+1][j-N+1] = temp;
-                            temp = tmp;
-                        }
-                        else { // 예외 상황 아니면, temp 값을 현재 값에
-                            int tmp = map[i][j+1];
-                            map[i][j+1] = temp;
-                            temp = tmp;
-                        }
-                    }
-                }
-                else {
-                    for(int j=0;j<N;j++) {
-                        // 좌측 끝에 도달했으므로, 위에 값과 변경
-                        if(j == N-1) {
-                            map[i-1][j-N+1] = temp;
-                        }
-                        else {
-                            int tmp = map[i][j+1];
-                            map[i][j+1] = temp;
-                            temp = tmp;
-                        }
-                    }
-                }
+            int tmp = u[N-1];
+
+            for(int i=N-1;i>=1;i--) {
+                u[i] = u[i-1];
             }
+            u[0] = d[N-1];
+            for(int i=N-1;i>=1;i--) {
+                d[i] = d[i-1];
+            }
+            d[0] = tmp;
+
             time++;
         }
 
-        for(int i=0;i<2;i++) {
-            for(int j=0;j<N;j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
+        for(int a : u) {
+            System.out.print(a + " ");
         }
+        System.out.println();
+        for(int b : d) {
+            System.out.print(b + " ");
+        }
+        System.out.println();
 
     }
 }
