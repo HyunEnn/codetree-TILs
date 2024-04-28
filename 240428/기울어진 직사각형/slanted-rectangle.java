7bf26eb56ca26e32a668f015c4c9af491a5eb41b
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-04-28 23:01:43 +0900

    Title: 기울어진 직사각형, Time: 227ms, Memory: 19MB, Status: Passed - CodeTree

--- 240428/기울어진 직사각형/slanted-rectangle.java
+++ 240428/기울어진 직사각형/slanted-rectangle.java
@@ -1,88 +1,60 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int N, cnt, loc;
-    static int[][] map;
-    static int max;
-    static boolean[][] v;
-    static int[] dr = {-1, -1, 1, 1};
-    static int[] dc = {1, -1, -1, 1};
-    static StringTokenizer st;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-        max = Integer.MIN_VALUE;
-        cnt = 0; // solve를 돌면서 값 체크
-        for(int i=0;i<N;i++) {
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+    public static final int DIR_NUM = 4;
+    public static final int MAX_N = 20;
 
-        for(int i=2;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                for(int h=1;h<=i;h++) {
-                    for(int w=1;w<=j;w++) {
-                        loc = 0;
-                        v = new boolean[N][N];
-                        solve(i, j, h, w);
-                        max = Math.max(max, cnt);
-                    }
-                }
-            }
-        }
+    public static int n;
+    public static int[][] grid = new int[MAX_N][MAX_N];
 
-        System.out.println(max);
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
     }
 
-    public static void solve(int r, int c, int h, int w) {
-        cnt = 0;
-        v[r][c] = true;
-        boolean flag = false;
-        // 0, 2는 h와 비교하고 1, 3은 w와 비교한다.
-        while(loc < 4) {
-            int ch = 0;
-            if(loc == 0 || loc == 2) {
-                while(ch < h) {
-                    int nr = r + dr[loc];
-                    int nc = c + dc[loc];
-                    if(nr < 0 || nr > N-1 || nc < 0 || nc > N-1 ) {
-                        if(ch == 0) {
-                            cnt = 0;
-                            return;
-                        }
-                        break;
-                    }
-                    
-                        cnt += map[nr][nc];
-                        r = nr;
-                        c = nc;
-                    
-                    ch++;
-                }
-            } else {
-                while(ch < w) {
-                    int nr = r + dr[loc];
-                    int nc = c + dc[loc];
-                    if(nr < 0 || nr > N-1 || nc < 0 || nc > N-1 ) {
-                        if(ch == 0) {
-                            cnt = 0;
-                            return;
-                        }
-                        break;
-                    }
-                    
-                        cnt += map[nr][nc];
-                        r = nr;
-                        c = nc;
-                    
-                    ch++;
-                }
+    public static int getScore(int x, int y, int k, int l) {
+        int[] dx = new int[]{-1, -1, 1, 1};
+        int[] dy = new int[]{1, -1, -1, 1};
+        int[] moveNum = new int[]{k, l, k, l};
+
+        int sumOfNums = 0;
+
+        // 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+        for(int d = 0; d < DIR_NUM; d++)
+            for(int q = 0; q < moveNum[d]; q++) {
+                x += dx[d]; y += dy[d];
+
+                // 기울어진 직사각형이 경계를 벗어나는 경우라면
+                // 불가능하다는 의미로 답이 갱신되지 않도록
+                // 0을 반환합니다.
+                if(!inRange(x, y))
+                    return 0;
+
+                sumOfNums += grid[x][y];
             }
-            loc++;
-        }
+
+        return sumOfNums;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                grid[i][j] = sc.nextInt();
+
+        int ans = 0;
+
+        // (i, j)를 시작으로 1, 2, 3, 4 방향
+        // 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+        // 기울어진 직사각형을 잡아보는
+        // 완전탐색을 진행해봅니다.
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                for(int k = 1; k < n; k++)
+                    for(int l = 1; l < n; l++)
+                        ans = Math.max(ans, getScore(i, j, k, l));
+
+        System.out.print(ans);
     }
 }
