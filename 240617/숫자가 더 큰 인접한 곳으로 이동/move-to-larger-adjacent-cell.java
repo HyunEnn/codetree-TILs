d54b16bd96a408796f20ef007cc6a41683ba583f
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-06-17 18:55:19 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 124ms, Memory: 9MB, Status: Passed - CodeTree

--- 240617/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
+++ 240617/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.java
@@ -7,7 +7,6 @@ public class Main {
     static int[] dr = {-1, 1, 0, 0}; // 상 하 좌 우
     static int[] dc = {0, 0, -1, 1};
     static List<Integer> list = new ArrayList<>();
-    static List<Integer> result = new ArrayList<>();
     static int[][] map;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
@@ -27,36 +26,31 @@ public class Main {
         }
 
         list.add(map[R][C]);
-        move(R, C, 0);
-        for(int i : result) {
+        move(R, C);
+        for(int i : list) {
             System.out.print(i + " ");
         }
     }
 
-    public static void move(int r, int c, int cnt) {
+    public static void move(int r, int c) {
 
         boolean flag = false;
 
+        int nr = 0;
+        int nc = 0;
         for(int k=0;k<4;k++) {
-            int nr = r + dr[k];
-            int nc = c + dc[k];
+            nr = r + dr[k];
+            nc = c + dc[k];
             if(nr < 0 || nr > N-1 || nc < 0 || nc > N-1 || map[r][c] >= map[nr][nc])
                 continue;
             list.add(map[nr][nc]);
             flag = true;
-            move(nr, nc, cnt + 1);
-            list.remove(list.size() - 1);
+            break;
         }
 
-        if(!flag) {
-            if(cnt > max) {
-                max = cnt;
-                result = new ArrayList<>();
-                for(int i : list)
-                    result.add(i);
-            }
+        if(flag) {
+            move(nr, nc);
         }
-
         // 4방 탐색을 마쳐도 true가 안됬으면 큰 수가 없다.
     }
 }
