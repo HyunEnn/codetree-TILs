7161bb994c7238e1c031b857c61af729556e19c5
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-07-19 14:55:39 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240719/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Pair {
+        int idx, val;
+        Pair(int idx, int val) {
+            this.idx = idx;
+            this.val = val;
+        }
+    }
+    static int N;
+    static int ans = Integer.MAX_VALUE;
+    static int[] arr;
+    static List<Integer> selectedA = new ArrayList<>();
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N*2];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N*2;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // N개씩 2개의 그룹으로 나눈다.
+        // 나눈 그룹 간의 차가 최소가 되는 것을 구한다.
+        // 예시로, [1,3,4]를 선택했다면 [2,5,6]을 고르게 해야함.
+        // 이 상태에서 원소간의 차를 계산하자.
+        for(int i=0;i<N*2;i++)
+            recursive(0, i);
+        System.out.println(ans);
+    }
+
+    public static void recursive(int idx, int curr) {
+
+        if(idx == N) {
+            ans = Math.min(ans, calculate());
+            return;
+        }
+
+        if(curr >= N*2) {
+            return;
+        }
+
+        // 선택했다면
+        selectedA.add(arr[curr]);
+        recursive(idx + 1, curr + 1);
+        selectedA.remove(selectedA.size() - 1);
+
+        // 선택 안하면
+        recursive(idx, curr + 1);
+    }
+
+    public static int calculate() {
+
+        // 1,3,5를 선택했다면 2,4,6을 골라야 하는데 어떻게?
+        List<Integer> unselectedB = new ArrayList<>();
+        for(int i=0;i<N*2;i++) {
+            if(!selectedA.contains(arr[i]))
+                unselectedB.add(arr[i]);
+        }
+
+        int sumA = 0, sumB = 0;
+        for(int i=0;i<N;i++) {
+            sumA += selectedA.get(i);
+            sumB += unselectedB.get(i);
+        }
+
+        return (int)Math.abs(sumA - sumB);
+    }
+}
