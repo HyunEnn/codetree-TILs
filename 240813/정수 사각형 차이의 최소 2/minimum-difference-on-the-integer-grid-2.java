fcd2924945d0d0ebb87e367598477e6c502e194e
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-08-13 18:56:23 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240813/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
+++ 240813/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.java
@@ -7,33 +7,56 @@ public class Main {
     static int N;
     static int[][] map;
     static int[][] dp;
+    static int res;
     static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
-        dp = new int[N][N];
+        res = Integer.MAX_VALUE;
         int min = Integer.MAX_VALUE;
         for(int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<N;j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 min = Math.min(min, map[i][j]);
-                dp[i][j] = -1;
             }
         }
+
+        for(int lower=1;lower<=100;lower++) {
+            int upper = solve(lower);
+
+            if(upper == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            res = Math.min(res, upper - lower);
+        }
+        System.out.println(res);
+    }
+
+    public static int solve(int lower) {
+        for(int i=0;i<N;i++) { // 여기서 map 을 최솟값 기준으로 탐색 안하는 것들은 제거
+            for(int j=0;j<N;j++) {
+                if(map[i][j] < lower)
+                    map[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        dp = new int[N][N]; // dp를 전부 초기화
+
         // 오른쪽 아래 이동에 대한 값
         dp[0][0] = map[0][0];
         for(int i=1;i<N;i++) {
             dp[i][0] = Math.max(dp[i-1][0], map[i][0]);
             dp[0][i] = Math.max(dp[0][i-1], map[0][i]);
         }
-
+        // 나머지에 대한 dp 이동 값 계산
         for(int i=1;i<N;i++) {
             for(int j=1;j<N;j++) {
                 dp[i][j] = Math.max(Math.min(dp[i-1][j], dp[i][j-1]), map[i][j]);
             }
         }
-        System.out.println(dp[N-1][N-1] - min);
+        return dp[N-1][N-1];
     }
 }
