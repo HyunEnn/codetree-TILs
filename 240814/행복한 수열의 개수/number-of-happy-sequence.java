eed9ed54046a4f704c03752022961e0bed6d0ab4
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-08-14 17:15:29 +0900

    Title: 행복한 수열의 개수, Time: 112ms, Memory: 10MB, Status: Passed - CodeTree

--- 240814/행복한 수열의 개수/number-of-happy-sequence.java
+++ 240814/행복한 수열의 개수/number-of-happy-sequence.java
@@ -24,32 +24,30 @@ public class Main {
         // 1행 r, c 를 기준으로 총 6번의 탐색을 진행하면 됨
         // 현재 행 기준 탐색
         for(int i=0;i<N;i++) {
-            int curr = map[i][0];
             int cnt = 1;
+            int saveCnt = 1;
             for(int j=1;j<N;j++) {
-                if(curr == map[i][j])
+                if(map[i][j-1] == map[i][j])
                     cnt++;
-                else {
-                    curr = map[i][j];
+                else
                     cnt = 1;
-                }
+                saveCnt = Math.max(saveCnt, cnt);
             }
-            if(cnt >= M)
+            if(saveCnt >= M)
                 res++;
         }
         // 열 기준 탐색
         for(int j=0;j<N;j++) {
-            int curr = map[0][j];
             int cnt = 1;
+            int saveCnt = 1;
             for(int i=1;i<N;i++) {
-                if(curr == map[i][j])
+                if(map[i-1][j] == map[i][j])
                     cnt++;
-                else {
-                    curr = map[i][j];
+                else
                     cnt = 1;
-                }
+                saveCnt = Math.max(saveCnt, cnt);
             }
-            if(cnt >= M)
+            if(saveCnt >= M)
                 res++;
         }
 
