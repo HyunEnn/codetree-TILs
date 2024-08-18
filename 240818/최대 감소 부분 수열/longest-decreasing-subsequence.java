3fe3cd41ad77c062cf821d955bfa130c05ca3322
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-08-18 16:06:37 +0900

    Title: 최대 감소 부분 수열, Time: 116ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240818/최대 감소 부분 수열/longest-decreasing-subsequence.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] arr;
+    static int[] dp;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        dp = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<=N;i++) {
+            for(int j=0;j<i;j++) {
+                if(arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int res = 0;
+        for(int i=0;i<=N;i++) {
+            res = Math.max(res, dp[i]);
+        }
+        System.out.println(res);
+    }
+}
