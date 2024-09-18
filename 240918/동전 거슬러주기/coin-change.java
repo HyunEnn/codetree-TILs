d5cabd8c76f37d302df3bfe62a7900865d0e670c
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-09-18 18:30:18 +0900

    Title: 동전 거슬러주기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240918/동전 거슬러주기/coin-change.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int[] dp;
+    static int INT_MAX = Integer.MAX_VALUE;
+    static int[] coins;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        coins = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
+        } 
+
+        initDP();
+        for(int i=1;i<=M;i++) {
+            for(int j=0;j<N;j++) {
+                if(i >= coins[j]) {
+                    if(dp[i - coins[j]] == INT_MAX) 
+                        continue;
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]] + 1);
+                }
+            }
+        }
+
+        System.out.println(dp[M]);
+    }
+
+    public static void initDP() {
+        dp = new int[M+1];
+        for(int i=1;i<=M;i++) {
+            dp[i] = INT_MAX;
+        }
+    }
+}
