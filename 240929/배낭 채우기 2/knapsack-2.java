9ee8cfb7bde47a2882afc4911a1c936e92997adc
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-09-29 22:07:13 +0900

    Title: 배낭 채우기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240929/배낭 채우기 2/knapsack-2.java
@@ -0,0 +1,40 @@
+public class Main {
+    static int N, M;
+    static StringTokenizer st;
+    static int[][] dp;
+    static int[] weights;
+    static int[] values;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        weights = new int[N+1];
+        values = new int[N+1];
+        dp = new int[N+1][M+1];
+
+        initDP();
+        // 선택하는 경우
+        for(int i=1;i<=N;i++) {
+            // 가방의 최대 무게
+            for(int j=1;j<=M;j++) {
+                int currWeight = weights[i];
+                int currValue = values[i];
+                if(j >= currWeight) {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-currWeight] + currValue);
+                }
+                else 
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    static void initDP() {
+        for(i=0;i<N;i++) {
+            for(int j=0;j<=M;j++) {
+                dp[i][j] = 0;
+            }
+        }
+    }
+}
