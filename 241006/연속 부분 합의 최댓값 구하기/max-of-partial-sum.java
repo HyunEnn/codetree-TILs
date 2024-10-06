faa19df8677848d25c443dfbe3195b1b637bfb13
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-10-06 21:19:35 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241006/연속 부분 합의 최댓값 구하기/max-of-partial-sum.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] arr;
+    static int[] dp;
+    static StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=N;i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[N+1];
+        initDP();
+        for(int i=2;i<=N;i++) {
+            // 이전 값의 합과 새로 시작하는 것중에 큰 것을 고름
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i=1;i<=N;i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+
+    static void initDP() {
+        dp[1] = 2 * arr[1];
+    }
+}
