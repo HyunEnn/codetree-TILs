8790ff6985839d4c79c1bdabb48c1243c44019a4
HYUNJUN, KIM <87894432+HyunEnn@users.noreply.github.com>
2024-11-05 15:28:32 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 445ms, Memory: 23MB, Status: Passed - CodeTree

--- 241105/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
+++ 241105/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.java
@@ -4,39 +4,52 @@ import java.io.*;
 public class Main {
     static class Point {
         int r, c;
+
         Point(int r, int c) {
             this.r = r;
             this.c = c;
         }
     }
+
     static int N;
     static int[][] map;
     static int[][] dp;
+    static boolean[][] v;
     static int[] dr = {-1, 0, 1, 0};
     static int[] dc = {0, 1, 0, -1};
     static StringTokenizer st;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N][N];
         dp = new int[N][N];
-        for(int i=0;i<N;i++) {
+        v = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<N;j++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
-                if(dp[i][j] == 0)
-                    bfs(i, j);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (dp[i][j] == 0) {
+                    dfs(i, j);
+                }
             }
         }
 
+//        for (int i = 0; i < N; i++) {
+//            for (int j = 0; j < N; j++) {
+//                System.out.print(dp[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
         int max = 0;
-        for(int i=0;i<N;i++) {
-            for(int j=0;j<N;j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 max = Math.max(max, dp[i][j]);
             }
         }
@@ -48,26 +61,22 @@ public class Main {
     }
 
     public static boolean canGo(int r, int c, int nr, int nc) {
-        if(!inRange(nr, nc))
+        if (!inRange(nr, nc))
             return false;
-        if(map[nr][nc] > map[r][c])
+        if (map[nr][nc] > map[r][c] && !v[nr][nc])
             return true;
         return false;
     }
 
-    public static void bfs(int r, int c) {
+    public static void dfs(int r, int c) {
+        if(dp[r][c] != 0) return;
         dp[r][c] = 1;
-        Queue<Point> Q = new ArrayDeque<>();
-        Q.offer(new Point(r, c));
-        while(!Q.isEmpty()) {
-            Point p = Q.poll();
-            for(int k=0;k<4;k++) {
-                int nr = p.r + dr[k];
-                int nc = p.c + dc[k];
-                if(canGo(p.r, p.c, nr, nc)) {
-                    dp[nr][nc] = Math.max(dp[nr][nc], dp[p.r][p.c] + 1);
-                    Q.offer(new Point(nr, nc));
-                }
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+            if (canGo(r, c, nr, nc)) {
+                if(dp[nr][nc] == 0) dfs(nr, nc);
+                dp[r][c] = Math.max(dp[r][c], dp[nr][nc] + 1);
             }
         }
     }
